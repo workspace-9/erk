27eccd18c9f60714069d519597b5946dcdfa7bb1
Jordan Bonecutter <i@jordanbonecutter.com>
2024-09-01 13:54:55 -0700

    first commit

---
+++ errors.go
@@ -0,0 +1,17 @@
+package errors
+
+// Try assumes that err is nil and returns t, otherwise it panics.
+func Try[T any](t T, err error) T {
+	if err != nil {
+		panic(err)
+	}
+
+	return t
+}
+
+// Must assumes that err is nil, otherwise it panics.
+func Must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
